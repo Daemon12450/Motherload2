45f58e8e2ef4a1f6e65975de1494f3d3e41040fd
AdamLewis005 <adam.lewis005@gmail.com>
2023-12-26 11:01:29 +0100

    ajout marqueur

---
+++ app/src/main/java/com/example/motherload2/Character/Upgrades.kt
@@ -0,0 +1,17 @@
+package com.example.motherload2.Character
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+class Upgrades(id:Int) {
+
+    internal val pick_id = id
+    internal var items = listOf<Item>()
+    private val _item = MutableLiveData<List<Item>>()
+    val item: LiveData<List<Item>> get() = _item
+
+    fun additems(item : Item){
+        this.items += listOf(item)
+        _item.postValue(this.items)
+    }
+}
